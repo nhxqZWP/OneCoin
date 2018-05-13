e2169b7bb5851a927bf787fc2c376897dac9d64f
nihaoZWP <719899730@qq.com>
2018-05-13 21:22:37 +0800

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,7 +11,7 @@ class IndexController extends Controller
         $key = config('platform.binance.key');
         $secret = config('platform.binance.secret');
         $api = new Binance($key, $secret);
-        $data = $api->time();
+        $data = $api->depth("BTCUSDT");;
         dd($data);
     }
 }
