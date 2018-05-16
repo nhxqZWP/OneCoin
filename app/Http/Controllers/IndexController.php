dcd4a5c8ee7587b24e6936ff511c1a033f3188fe
nihaoZWP <719899730@qq.com>
2018-05-16 23:38:35 +0800

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Services\Binance;
+use App\Http\Services\BinanceService;
 
 class IndexController extends Controller
 {
     public function getIndex()
     {
+        $data = BinanceService::getMACD();
+        dd($data);
+
         $key = config('platform.binance.key');
         $secret = config('platform.binance.secret');
         $api = new Binance($key, $secret);
--- app/Http/Services/BinanceService.php
+++ app/Http/Services/BinanceService.php
@@ -7,7 +7,24 @@ class BinanceService
     public static function getMACD($pair = 'BTCUSDT', $period = '1h', $short=12,$long=26,$m=9)
     {
         //Periods: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
-
+        $kList = self::getCandleSticks($pair, $period);
+        $kStockList = [];
+        foreach ($kList as $k => $v) {
+            if ($k == 0) {
+                $kStockList[$k]['ema12'] = $v['close'];
+                $kStockList[$k]['ema26'] = $v['close'];
+                $kStockList[$k]['dif'] = 0;
+                $kStockList[$k]['dea'] = 0;
+                $kStockList[$k]['macd'] = 0;
+            } else {
+                $kStockList[$k]['ema12'] = (2.0 * $v['close'] + ($short-1) * $kStockList[$k-1]['ema12']) / ($short+1);
+                $kStockList[$k]['ema26'] = (2.0 * $v['close'] + ($long-1) * $kStockList[$k-1]['ema26']) / ($long+1);
+                $kStockList[$k]['dif'] = $kStockList[$k]['ema12'] - $kStockList[$k]['ema26'];
+                $kStockList[$k]['dea'] = (2.0 * $kStockList[$k]['dif'] + ($m-1)*$kStockList[$k-1]['dea']) / ($m+1);
+                $kStockList[$k]['macd'] = 2.0 * ($kStockList[$k]['dif'] - $kStockList[$k]['dea']);
+            }
+        }
+        return $kStockList;
     }
 
     public static function getEMA($param)
@@ -21,7 +38,7 @@ class BinanceService
         $secret = config('platform.binance.secret');
         $api = new Binance($key, $secret);
         $data = $api->candlesticks($pair, $period);;
-        if (is_array($data)) return $data;
+        if (is_array($data)) return array_values($data);
         return null;
     }
 }
