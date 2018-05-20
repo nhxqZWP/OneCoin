84b8650a2b5ea53de67e134d058254ef447f6c81
nihaoZWP <719899730@qq.com>
2018-05-20 15:41:48 +0800

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,9 +12,13 @@ class IndexController extends Controller
         $key = config('platform.binance.key');
         $secret = config('platform.binance.secret');
         $api = new Binance($key, $secret);
-        $price = $api->prices()['BTCUSDT'];
+        $price = $api->prices();
         var_dump($price);
-        $data = $api->candlesticks("BTCUSDT", '30m');
+
+        $data = BinanceService::getMACD($pair = 'BTCUSDT', $period = '30m');
+        dd(array_reverse($data));
+
+        $data = $api->candlesticks("BTCUSDT", '1h');
         $data = array_values($data);
         dd($data);
     }
