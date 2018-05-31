ef1c1c55daffe6b5eb1e60ec7afc58b4be99f684
nihaoZWP <719899730@qq.com>
2018-06-01 00:29:19 +0800

    update

--- app/Http/Services/BinanceService.php
+++ app/Http/Services/BinanceService.php
@@ -138,7 +138,8 @@ class BinanceService
         if (is_null($status)) $status = 0;
 
         // 小于零值变大时买入
-        if ($preMacd < 0 && $newMacd < 0 && $newMacd > $preMacd && $status != 1) {
+//        if ($preMacd < 0 && $newMacd < 0 && $newMacd > $preMacd && $status != 1) {
+        if ($newMacd > $preMacd && $status != 1) {
             $buyBtc = TradeRecord::createBuyOrder($usdt, $pair);
             Redis::set('2_binance:btc', $buyBtc);
             Redis::set('2_binance:usdt', 0);
@@ -147,7 +148,7 @@ class BinanceService
         }
 
         // 值变小时卖出
-        if ($status == 1) {
+        if (intval($status) == 1) {
             if ($newMacd < $preMacd) {
                 $sellUsdt = TradeRecord::createSellOrder($btc, $pair);
                 Redis::set('2_binance:btc', 0);
